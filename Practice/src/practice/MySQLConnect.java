6575dd01ac22c83400d5f441b8d0b25d1cd27445
sarker306@gmail.com <sarker306@gmail.com@cc688fa2-060b-5eac-bfdd-b345d1526dc6>
2012-07-05 22:45:16 +0000

    Testing MySQL again with local version of MySQL, the next revisions will be strict.

--- Practice/src/practice/MySQLConnect.java
+++ Practice/src/practice/MySQLConnect.java
@@ -19,20 +19,20 @@ public class MySQLConnect
        try
        {
            String userName = "root";
-           String password = "root";
+           String password = "admin";
            String url = "jdbc:mysql://localhost/test";
            Class.forName ("com.mysql.jdbc.Driver").newInstance ();
            conn = DriverManager.getConnection (url, userName, password);
            System.out.println ("Database connection established");
-           Statement s = conn.createStatement ();
+           Statement stmnt = conn.createStatement ();
            int count;
-           s.executeUpdate ("DROP TABLE IF EXISTS animal");
-           s.executeUpdate (
+           stmnt.executeUpdate ("DROP TABLE IF EXISTS animal");
+           stmnt.executeUpdate (
                        "CREATE TABLE animal ("
                        + "id INT UNSIGNED NOT NULL AUTO_INCREMENT,"
                        + "PRIMARY KEY (id),"
                        + "name CHAR(40), category CHAR(40))");
-           count = s.executeUpdate (
+           count = stmnt.executeUpdate (
                        "INSERT INTO animal (name, category)"
                        + " VALUES"
                        + "('snake', 'reptile'),"
@@ -40,34 +40,34 @@ public class MySQLConnect
                        + "('tuna', 'fish'),"
                        + "('racoon', 'mammal')");
             System.out.println (count + " rows were inserted");
-           PreparedStatement ps;
-           ps = conn.prepareStatement (
+           PreparedStatement preparedStmt;
+           preparedStmt = conn.prepareStatement (
                        "INSERT INTO animal (name, category) VALUES(?,?)");
-           ps.setString (1, "crow");
-           ps.setString (2, "bird");
-           count = ps.executeUpdate ();
-           ps.close ();
+           preparedStmt.setString (1, "crow");
+           preparedStmt.setString (2, "bird");
+           count = preparedStmt.executeUpdate ();
+           preparedStmt.close ();
            //s.close ();
            System.out.println (count + " rows were inserted");
-           s.executeQuery ("SELECT id, name, category FROM animal");
-           ResultSet rs = s.getResultSet ();
+           stmnt.executeQuery ("SELECT id, name, category FROM animal");
+           ResultSet resultSet = stmnt.getResultSet ();
            count = 0;
-           while (rs.next ())
+           while (resultSet.next ())
            {
-               int idVal = rs.getInt ("id");
-               String nameVal = rs.getString ("name");
-               String catVal = rs.getString ("category");
+               int idVal = resultSet.getInt ("id");
+               String nameVal = resultSet.getString ("name");
+               String catVal = resultSet.getString ("category");
                System.out.println (
                        "id = " + idVal
                        + ", name = " + nameVal
                        + ", category = " + catVal);
                ++count;
            }
-           rs.close ();
+           resultSet.close ();
            System.out.println (count + " rows were retrieved");
-           s.execute("DROP TABLE animal");
+           stmnt.execute("DROP TABLE animal");
            System.out.println("Database animal successfully deleted");
-           s.close();
+           stmnt.close();
        }
        catch (Exception e)
        {
